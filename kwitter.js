c3e777b37563fc9f898e8a45614e99becfeb0efe
yashasKawankar <90907007+yashasKawankar@users.noreply.github.com>
2023-12-20 13:55:15 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,6 +1,6 @@
 function adduser(){
 
-username=docuent.getelementbyid("user_name").value;
-localStorage.setItem("user_name",username);
-window.location="kwitter_room.html"
+    user_name=document.getElementById("user_name").value;
+    localStorage.setitem("user_name",user_name);
+    windowlocation="kwitter_room.html";
 }
